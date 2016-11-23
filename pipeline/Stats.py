f79ecdfd00a70767dd1fde066f9d9816242ae531
cagdasyelen <cagdas@utexas.edu>
2016-11-23 02:30:22 -0600

    pandas describe statistics added

--- pipeline/Stats.py
+++ pipeline/Stats.py
@@ -6,45 +6,35 @@ class Stats(object):
 
 
     #init to 0 for all attributes
-    accAvgX = 0
-    accStdDevX = 0
-    accMinX = 0
-    accMaxX = 0
-    accAvgY = 0
-    accStdDevY = 0
-    accMinY = 0
-    accMaxY = 0
-    accAvgZ = 0
-    accStdDevZ = 0
-    accMinZ = 0
-    accMaxZ = 0
-
-    accAvgMag = 0
-    accStdDevMag = 0
-    accMinMag = 0
-    accMaxMag = 0
+    #TODO: implement magnitude fields and methods
+    accX = pd.DataFrame()
+    accY = pd.DataFrame()
+    accZ = pd.DataFrame()
+    accMag = pd.DataFrame()
 
 
-    gyroAvgX = 0
-    gyroStdDevX = 0
-    gyroMinX = 0
-    gyroMaxX = 0
-    gyroAvgY = 0
-    gyroStdDevY = 0
-    gyroMinY = 0
-    gyroMaxY = 0
-    gyroAvgZ = 0
-    gyroStdDevZ = 0
-    gyroMinZ = 0
-    gyroMaxZ = 0
-
-    gyroAvgMag = 0
-    gyroStdDevMag = 0
-    gyroMinMag = 0
-    gyroMaxMag = 0
+    gyroX = pd.DataFrame()
+    gyroY = pd.DataFrame()
+    gyroZ = pd.DataFrame()
+    gyroMag = pd.DataFrame()
 
     def __init__(self, filePath):
         self.df = pd.read_csv(filePath, header=0, index_col=0)
+
+        self.statsDf = self.df.describe()
+
+        self.accX = self.statsDf['ax']
+        self.accY = self.statsDf['ay']
+        self.accZ = self.statsDf['az']
+
+        self.gyroX = self.statsDf['gx']
+        self.gyroY = self.statsDf['gy']
+        self.gyroZ = self.statsDf['gz']
+
+
+
+
+
 
 
 
