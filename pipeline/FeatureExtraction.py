431a8d3d24cb58457fbd7083be4a70240ea85717
cagdasyelen <cagdas@utexas.edu>
2016-11-25 21:20:35 -0600

    some features are added

--- pipeline/FeatureExtraction.py
+++ pipeline/FeatureExtraction.py
@@ -10,8 +10,15 @@ the features prior to the classification stage
 
 Features to be extracted from raw data:
 
-	-Magnitude of acc
-	-Magnitude of gyro
+	-min(aMag)
+	-max(aMag)
+	-mean(aMag)
+	-std(aMag)
+	-min(gMag)
+	-max(gMag)
+	-mean(gMag)
+	-std(gMag)
+
 
 '''
 
@@ -22,9 +29,52 @@ class FeatureExtraction:
 		self.rawDf = pd.read_csv(fileDir, header=0, index_col=0)
 
 		#Adding mag of acc and gyro to pandas frame
-
 		self.rawDf["aMag"] = ( self.rawDf["ax"]**2 + self.rawDf["ay"]**2 + self.rawDf["az"]**2 )**(0.5)
 		self.rawDf["gMag"] = ( self.rawDf["gx"]**2 + self.rawDf["gy"]**2 + self.rawDf["gz"]**2 )**(0.5)
+
+		#calculate basic stats once and use later
+		self.stats = self.rawDf.describe()
+
+		#features init
+		self.features = {}
+
+
+		#put the label for different severity levels based on the filename
+		if(fileDir[len(fileDir) - 5] == "1"):
+			self.label = 1
+		elif(fileDir[len(fileDir) - 5] == "2"):
+			self.label = 2
+		elif(fileDir[len(fileDir) - 5] == "3"):
+			self.label = 3
+		else:
+			self.label = 0
+
+		
+
+
+
+	def addFeatures(self,addMin,addMax,addAvg,addStd):
+
+		if(addMin == True):
+			self.features['aMin'] = self.stats["aMag"]["min"]
+			self.features['gMin'] = self.stats["gMag"]["min"]
+
+		if(addMax == True):
+			self.features['aMax'] = self.stats["aMag"]["max"]
+			self.features['gMax'] = self.stats["gMag"]["max"]
+
+		if(addAvg == True):
+			self.features['aAvg'] = self.stats["aMag"]["mean"]
+			self.features['gAvg'] = self.stats["gMag"]["mean"]
+
+		if(addStd == True):
+			self.features['aStd'] = self.stats["aMag"]["std"]
+			self.features['gStd'] = self.stats["gMag"]["std"]
+
+
+		print(self.features)
+	
+
 
 		
 	def getAccMag(self):
@@ -41,7 +91,10 @@ class FeatureExtraction:
 
 
 
-f = FeatureExtraction("../raw-data/sample23_fall0.csv")
+
+
+f = FeatureExtraction("../raw-data/sample32_fall0.csv")
+f.addFeatures(True, True, True, True)
 
 
 
