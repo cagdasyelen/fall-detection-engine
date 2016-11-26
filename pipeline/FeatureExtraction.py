66b03cd3cb05a40bbc62854f64b2704d46422751
cagdasyelen <cagdas@utexas.edu>
2016-11-25 18:55:34 -0600

    Feature extraction init

---
+++ pipeline/FeatureExtraction.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+
+
+'''
+
+This class gets the raw data and extracts 
+the features prior to the classification stage
+
+Features to be extracted from raw data:
+
+
+
+'''
+
+class FeatureExtraction:
+
+	def __init__(self, dir):
+		self.rawDataDir = dir
+
+
+f = FeatureExtraction("../raw-data")
+
+
+
+
+
+
+
+
